8932d7263b3a7779c5badcf95328a4aa55a65858
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-04-22 13:40:38 +0200

    CPP: Use lib-givaro to compute integer in a finite field

--- dualec-cpp/src/main/dualec.cpp
+++ dualec-cpp/src/main/dualec.cpp
@@ -1,6 +1,15 @@
 #include "dualec.h"
 #include <iostream>
+#include <givaro/modular-integer.h>
 
+typedef Givaro::Modular<Givaro::Integer> Zp;
+typedef Zp::Element Element; // an element in Zp
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    auto ffield = Zp(123);
+    Element element_mod_zp;
+    ffield.init(element_mod_zp, 325);
+    std::cout << "325 % 123 = 79, actual: " << element_mod_zp << std::endl;
+    Element product;
+    ffield.mul(product, element_mod_zp, Element(2));
+    std::cout << "79 * 2 % 123 = 35, actual: " << product << std::endl;
 }
