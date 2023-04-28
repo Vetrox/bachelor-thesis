6e7d0e7841250a6f23937b56e5f55469f2bf02cd
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-04-28 19:51:03 +0200

    CPP: Add hex printing for byte array

--- dualec-cpp/src/main/forward.cpp
+++ dualec-cpp/src/main/forward.cpp
@@ -1,8 +1,17 @@
 #include "forward.h"
+#include <iomanip>
 
 std::string bigint_hex(BigInt const& i)
 {
     char* hex_str;
     gmp_asprintf(&hex_str, "%#Zx", i.get_mpz_const());
     return hex_str;
+}
+
+std::string bytes_as_hex(std::span<uint8_t> const& bytes)
+{
+    std::stringstream ss;
+    for (auto const& word : bytes)
+        ss << std::hex << std::setw(2) << std::setfill('0') << +word;
+    return ss.str();
 }
--- dualec-cpp/src/main/forward.h
+++ dualec-cpp/src/main/forward.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <cstdint>
 #include <givaro/modular-integer.h>
+#include <span>
 
 typedef Givaro::Integer BigInt;
 typedef Givaro::Modular<BigInt> Zp;
 typedef Zp::Element Element; // an element in Zp
 
 std::string bigint_hex(BigInt const& i);
+std::string bytes_as_hex(std::span<uint8_t> const&);
