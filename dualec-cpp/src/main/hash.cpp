1c7c3056e2aade4beb9cf14fa1130ed2a4652372
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-05-01 13:04:29 +0200

    CPP: Hash remove dependencies

--- dualec-cpp/src/main/hash.cpp
+++ dualec-cpp/src/main/hash.cpp
@@ -1,8 +1,5 @@
 #include "hash.h"
 #include "../../sha256/include/SHA256.h"
-#include <algorithm>
-#include <cstdint>
-#include <memory>
 
 static SHA256 sha256;
 BitStr SHA256_Hash(BitStr const& input)
@@ -10,9 +7,5 @@ BitStr SHA256_Hash(BitStr const& input)
     auto tmp = input.to_baked_array();
     sha256.update(tmp.data(), tmp.size());
     auto* digest = sha256.digest();
-    auto* box = new uint8_t[32];
-    std::copy(digest, digest + 32, box);
-    delete[] digest;
-    std::cout << "FINISHED HASHIN" << std::endl;
-    return BitStr(std::unique_ptr<uint8_t[]>(box), 32);
+    return BitStr(std::unique_ptr<uint8_t[]>(digest), 32);
 }
