955864b2c80b69ea2f0faee90f974b250fc8231e
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-05-29 15:10:48 +0200

    MBEDTLS: Cpp-ify server

--- mbedtls-server/ssl/ssl_server.cpp
+++ mbedtls-server/ssl/ssl_server.cpp
@@ -2,6 +2,7 @@
 #include "mbedtls/pk.h"
 #include "mbedtls/platform.h"
 
+#include <fstream>
 #include <stdlib.h>
 #include <string.h>
 
@@ -14,6 +15,9 @@
 #include "mbedtls/debug.h"
 #include "mbedtls/x509_crt.h"
 
+#include <iostream>
+#include <iomanip>
+
 #define HTTP_RESPONSE \
     "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n" \
     "<h2>mbed TLS Test Server</h2>\r\n" \
@@ -21,14 +25,14 @@
 
 #define DEBUG_LEVEL 4
 
-static void my_debug(void *ctx, int level,
+static void my_debug([[maybe_unused]] void *ctx, int level,
                      const char *file, int line,
                      const char *str)
 {
-    ((void) level);
-
-    mbedtls_fprintf((FILE *) ctx, "%s:%04d: %s", file, line, str);
-    fflush((FILE *) ctx);
+    std::cout << std::string(file).substr(38) << ":" << std::setw(4) << std::dec << std::setfill('0') << line << ":";
+    for (int i = 0; i < level; ++i)
+        std::cout << ' ';
+    std::cout << str << std::flush;
 }
 
 int my_generate(void *p_rng, unsigned char *output, size_t output_len, const unsigned char *additional, size_t add_len)
@@ -70,74 +74,49 @@ int main(void)
     mbedtls_pk_init(&pkey);
     mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
-
     mbedtls_debug_set_threshold(DEBUG_LEVEL);
 
-    /*
-     * 1. Seed the RNG
-     */
-    mbedtls_printf("  . Seeding the random number generator...");
-    fflush(stdout);
-
+    // 1. Seed the RNG
+    std::cout << "Setting up the random number generator...";
     // NOTE: This call is the DRBG_Instantiate call in SP 800-90A
     if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-                                     (const unsigned char *) pers,
+                                     reinterpret_cast<const unsigned char *>(pers),
                                      strlen(pers))) != 0) {
-        mbedtls_printf(" failed\n  ! mbedtls_ctr_drbg_seed returned %d\n", ret);
+        std::cout << " failed: mbedtls_ctr_drbg_seed returned " << ret << std::endl;
         goto exit;
     }
+    std::cout << " ok" << std::endl;
 
-    mbedtls_printf(" ok\n");
-
-    /*
-     * 2. Load the certificates and private RSA key
-     */
-    mbedtls_printf("\n  . Loading the server cert. and key...");
-    fflush(stdout);
-
-    /*
-     * This demonstration program uses embedded test certificates.
-     * Instead, you may want to use mbedtls_x509_crt_parse_file() to read the
-     * server and CA certificates, as well as mbedtls_pk_parse_keyfile().
-     */
+    // 2. Load the certificates and private RSA key
+    std::cout << "Loading the server certificate and key...";
     ret = mbedtls_x509_crt_parse_file(&srvcert, "server-cert.pem");
     if (ret != 0) {
-        mbedtls_printf(" failed\n  !  mbedtls_x509_crt_parse_file returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_x509_crt_parse_file returned " << ret << std::endl;
         goto exit;
     }
 
     ret = mbedtls_pk_parse_keyfile(&pkey, "server-private-key.pem", NULL, mbedtls_ctr_drbg_random, &ctr_drbg);
     if (ret != 0) {
-        mbedtls_printf(" failed\n  !  mbedtls_pk_parse_key returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_pk_parse_key returned " << ret << std::endl;
         goto exit;
     }
+    std::cout << " ok" << std::endl;
 
-    mbedtls_printf(" ok\n");
-
-    /*
-     * 3. Setup the listening TCP socket
-     */
-    mbedtls_printf("  . Bind on https://localhost:4433/ ...");
-    fflush(stdout);
-
+    // 3. Setup the listening TCP socket
+    std::cout << "Bind on https://localhost:4433/ ...";
     if ((ret = mbedtls_net_bind(&listen_fd, NULL, "4433", MBEDTLS_NET_PROTO_TCP)) != 0) {
-        mbedtls_printf(" failed\n  ! mbedtls_net_bind returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_net_bind returned " << ret << std::endl;
         goto exit;
     }
+    std::cout << " ok" << std::endl;
 
-    mbedtls_printf(" ok\n");
-
-    /*
-     * 4. Setup stuff
-     */
-    mbedtls_printf("  . Setting up the SSL data....");
-    fflush(stdout);
-
+    // 4. Setup
+    std::cout <<"Setting up the SSL data...";
     if ((ret = mbedtls_ssl_config_defaults(&conf,
                                            MBEDTLS_SSL_IS_SERVER,
                                            MBEDTLS_SSL_TRANSPORT_STREAM,
                                            MBEDTLS_SSL_PRESET_DEFAULT)) != 0) {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_config_defaults returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_ssl_config_defaults returned " << ret << std::endl;
         goto exit;
     }
 
@@ -146,16 +125,15 @@ int main(void)
 
     mbedtls_ssl_conf_ca_chain(&conf, srvcert.next, NULL);
     if ((ret = mbedtls_ssl_conf_own_cert(&conf, &srvcert, &pkey)) != 0) {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_conf_own_cert returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_ssl_conf_own_cert returned " << ret << std::endl;
         goto exit;
     }
 
     if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0) {
-        mbedtls_printf(" failed\n  ! mbedtls_ssl_setup returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_ssl_setup returned " << ret << std::endl;
         goto exit;
     }
-
-    mbedtls_printf(" ok\n");
+    std::cout << " ok" << std::endl;
 
 reset:
     if (ret != 0) {
@@ -165,46 +143,33 @@ reset:
     }
 
     mbedtls_net_free(&client_fd);
-
     mbedtls_ssl_session_reset(&ssl);
 
-    /*
-     * 3. Wait until a client connects
-     */
-    mbedtls_printf("  . Waiting for a remote connection ...");
-    fflush(stdout);
-
+    // 3. Wait until a client connects
+    std::cout << "Waiting for a remote connection ..." << std::flush;
     if ((ret = mbedtls_net_accept(&listen_fd, &client_fd,
                                   NULL, 0, NULL)) != 0) {
-        mbedtls_printf(" failed\n  ! mbedtls_net_accept returned %d\n\n", ret);
+        std::cout << " failed: mbedtls_net_accept returned " << ret << std::endl;
         goto exit;
     }
 
     mbedtls_ssl_set_bio(&ssl, &client_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
 
-    mbedtls_printf(" ok\n");
+    std::cout << " ok" << std::endl;
 
-    /*
-     * 5. Handshake
-     */
-    mbedtls_printf("  . Performing the SSL/TLS handshake...");
-    fflush(stdout);
-
+    // 5. Handshake
+    std::cout << "Performing the SSL/TLS handshake...";
     while ((ret = mbedtls_ssl_handshake(&ssl)) != 0) {
         if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-            mbedtls_printf(" failed\n  ! mbedtls_ssl_handshake returned %d\n\n", ret);
+            std::cout << " failed: mbedtls_ssl_handshake returned " << ret << std::endl;
             goto reset;
         }
     }
 
-    mbedtls_printf(" ok\n");
+    std::cout << " ok" << std::endl;
 
-    /*
-     * 6. Read the HTTP Request
-     */
-    mbedtls_printf("  < Read from client:");
-    fflush(stdout);
-
+    // 6. Read the HTTP Request
+    std::cout << "< Read from client:";
     do {
         len = sizeof(buf) - 1;
         memset(buf, 0, sizeof(buf));
@@ -225,7 +190,7 @@ reset:
                     break;
 
                 default:
-                    mbedtls_printf(" mbedtls_ssl_read returned -0x%x\n", (unsigned int) -ret);
+                    mbedtls_printf(" mbedtls_ssl_read returned -0x%x\n", static_cast<unsigned int>(-ret));
                     break;
             }
 
@@ -233,16 +198,14 @@ reset:
         }
 
         len = ret;
-        mbedtls_printf(" %d bytes read\n\n%s", len, (char *) buf);
+        std::cout << " " << len << "bytes read\n\n" << reinterpret_cast<char*>(buf);
 
         if (ret > 0) {
             break;
         }
     } while (1);
 
-    /*
-     * 7. Write the 200 Response
-     */
+    // 7. Write the 200 Response
     mbedtls_printf("  > Write to client:");
     fflush(stdout);
 
