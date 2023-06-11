02424f8defffa2ef2b3568cab020a9c304dd2651
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-06-11 15:55:47 +0200

    CPP: Implement adding in Jacobian coordinates

--- dualec-cpp/src/main/jacobi_elliptic_curve.h
+++ dualec-cpp/src/main/jacobi_elliptic_curve.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "affine_point.h"
 #include "forward.h"
 #include "jacobi_point.h"
 
@@ -75,6 +76,81 @@ public:
         // 18. Y3 <- T1 − Y3
         m_field.negin(Y3);
         m_field.addin(Y3, T1);
+
+        return JacobiPoint(X3, Y3, Z3, m_field);
+    }
+
+    JacobiPoint add(JacobiPoint const& P, AffinePoint const& Q)
+    {
+        if (Q.identity())
+            return JacobiPoint(P);
+        if (P.is_identity())
+            return JacobiPoint(Q.x(), Q.y(), 1, m_field);
+
+        Element T1, T2, T3, T4, X3, Y3, Z3;
+
+        // 3. T1 <- Z1^2
+        m_field.mul(T1, P.z(), P.z());
+
+        // 4. T2 <- T1 * Z1
+        m_field.mul(T2, T1, P.z());
+
+        // 5. T1 <- T1 * x2
+        m_field.mulin(T1, Q.x());
+
+        // 6. T2 <- T2 * y2
+        m_field.mulin(T2, Q.y());
+
+        // 7. T1 <- T1 − X1
+        m_field.subin(T1, P.x());
+
+        // 8. T2 <- T2 − Y1
+        m_field.subin(T2, P.y());
+
+        // 9. If T1 = 0 then
+        if (T1 == 0) {
+            /* 9.1 If T2 = 0 then use Algorithm 3.21 to compute (X3 : Y3 : Z3 ) = 2(x2 : y2 : 1) and return(X3 : Y3 : Z3 ). */
+            return _double(JacobiPoint(Q.x(), Q.y(), 1, m_field));
+        } else {
+            // else return Infinity.
+            return JacobiPoint(m_field);
+        }
+
+        // 10. Z3 <- Z1 * T1
+        m_field.mul(Z3, P.z(), T1);
+
+        // 11. T3 <- T1^2
+        m_field.mul(T3, T1, T1);
+
+        // 12. T4 <- T3 * T1
+        m_field.mul(T4, T3, T1);
+
+        // 13. T3 <- T3 * X1
+        m_field.mulin(T3, P.x());
+
+        // 14. T1 <- 2 * T3
+        m_field.mul(T1, 2, T3);
+
+        // 15. X3 <- T2^2
+        m_field.mul(X3, T2, T2);
+
+        // 16. X3 <- X3 − T1
+        m_field.subin(X3, T1);
+
+        // 17. X3 <- X3 − T4
+        m_field.subin(X3, T4);
+
+        // 18. T3 <- T3 − X3
+        m_field.subin(T3, X3);
+
+        // 19. T3 <- T3 * T2
+        m_field.mulin(T3, T2);
+
+        // 20. T4 <- T4 * Y1
+        m_field.mulin(T4, P.y());
+
+        // 21. Y3 <- T3 − T4
+        m_field.sub(Y3, T3, T4);
 
         return JacobiPoint(X3, Y3, Z3, m_field);
     }
