422a4908f7328e7f2237766b7891addf6c8d45c3
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-06-15 23:18:22 +0200

    MBEDTLS: Refactor dualec on the mbedtls-server

--- mbedtls-server/ssl/drbg.cpp
+++ mbedtls-server/ssl/drbg.cpp
@@ -1,3 +1,4 @@
+#include "dualec_curve.h"
 #include "mbedtls/entropy.h"
 #include <cstddef>
 #include <cstring>
@@ -5,9 +6,40 @@
 #include <optional>
 #include <dualec.h>
 
+static constexpr auto security_strength = 128; /* 128, 192, 256 */
 static std::optional<DEC::WorkingState> working_state;
+static DEC::Curve bad_curve = DEC::pick_curve(security_strength);
+
+#define LEAK std::cout << "[LEAK] "
+
+
+static void generate_dQ(AffinePoint const& P, BigInt const& order_of_p, JacobiEllipticCurve const& curve, BigInt& out_d, AffinePoint& out_Q)
+{
+    Zp order_field(order_of_p);
+    Givaro::RandomIntegerIterator<> random_integer_iterator(order_field);
+    while (true) {
+        // pick random d
+        out_d = random_integer_iterator.randomInteger();
+        if (Givaro::isZero(out_d))
+            continue;
+        // compute the inverse of d
+        BigInt e;
+        order_field.inv(e, out_d);
+        // compute Q based on P
+        curve.scalar(out_Q, P, e);
+
+        // perform sanity check
+        AffinePoint P2;
+        curve.scalar(P2, out_Q, out_d);
+        if (P2 == P)
+            return;
+    }
+}
 
 void init_working_state(mbedtls_entropy_context& entropy, std::string personalization_string) {
+    LEAK << "security_strength: " << security_strength << std::endl;
+    LEAK << "personalization_string: " << personalization_string << std::endl;
+
     auto* buf = entropy.accumulator.buffer; // if not initialized, let it segfault bc our application relies on it
     auto buf_len = entropy.accumulator.total[0]; // same here
     auto* buf_copy = new uint8_t[buf_len];
@@ -15,13 +47,17 @@ void init_working_state(mbedtls_entropy_context& entropy, std::string personaliz
 
     auto pers_copy_len = personalization_string.length();
     auto* pers_copy = new uint8_t[pers_copy_len];
+
+    BigInt d(-1);
+    generate_dQ(bad_curve.P, bad_curve.order_of_p, bad_curve.curve, d, bad_curve.Q);
+    LEAK << "Produced backdoor\n\tsecret_d: " << bigint_hex(d) << "\n\tdec_curve: " << bad_curve.to_string(10) << std::endl;
+
     std::copy(personalization_string.begin(), personalization_string.end(), pers_copy);
     working_state.emplace(DEC::Instantiate(
                 BitStr(std::unique_ptr<uint8_t[]>(buf_copy), buf_len), /* entropy input */
-                BitStr(0), /* nonce wasn't used very much, even mbedtls doesn't use nonces internally */
+                BitStr(0), /* mbedtls doesn't use nonces internally */
                 BitStr(std::unique_ptr<uint8_t[]>(pers_copy), pers_copy_len),
-                128) /* security strength. 128, 192, 256 */
-            );
+                security_strength, &bad_curve));
 }
 
 int my_generate(void *p_rng, unsigned char *output, size_t output_len, const unsigned char *additional, size_t add_len)
@@ -33,11 +69,14 @@ int my_generate(void *p_rng, unsigned char *output, size_t output_len, const uns
         abort();
     }
 
-    auto* buf = new uint8_t[add_len];
-    memcpy(buf, additional, add_len);
-    auto rt = DEC::Generate(working_state.value(),
-            output_len*8,
-            BitStr(std::unique_ptr<uint8_t[]>(buf), add_len));
+    auto adin = BitStr(BigInt(0), add_len);
+    if (additional && add_len > 0) {
+        auto* buf = new uint8_t[add_len];
+        memcpy(buf, additional, add_len);
+        adin = BitStr(std::unique_ptr<uint8_t[]>(buf), add_len);
+    }
+    LEAK << "adin: " << adin.as_hex_string() << std::endl;
+    auto rt = DEC::Generate(working_state.value(), output_len*8, std::move(adin));
     auto baked_rt = rt.to_baked_array();
     if (baked_rt.size() != output_len) {
         std::cout << "FATAL ERROR: Baked array was not the requested size" << std::endl;
