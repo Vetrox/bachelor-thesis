0e6ccec719cf48f32a9ff15fd84718dc61527905
Vetrox <39677514+Vetrox@users.noreply.github.com>
2023-06-15 20:26:37 +0200

    TLS: Fill in DEC::Curve missing parameters

--- tls-attack/src/input.cpp
+++ tls-attack/src/input.cpp
@@ -1,18 +1,24 @@
 #include "input.h"
+#include "affine_point.h"
 #include "jacobi_elliptic_curve.h"
 
 Input setup_input()
 {
     return {
-    .server_random = {},
-    .client_random = {},
-    .dh_generator = 0,
-    .dh_prime = 0,
-    .dh_bitlen_of_a = 0,
-    .dh_pubkey_server = 0,
-    .dh_pubkey_client = 0,
-    .dec_security_strength = 128,
-    .dec_curve = JacobiEllipticCurve(0,0),
-    .dec_secret_d = 0
-};
+        .server_random = {},
+        .client_random = {},
+        .dh_generator = 0,
+        .dh_prime = 0,
+        .dh_bitlen_of_a = 0,
+        .dh_pubkey_server = 0,
+        .dh_pubkey_client = 0,
+        .dec_security_strength = 128,
+        .dec_curve = {
+            .curve = JacobiEllipticCurve(0,0),
+            .P = AffinePoint(0,0),
+            .order_of_p = 0,
+            .Q = AffinePoint(0,0),
+        },
+        .dec_secret_d = 0
+    };
 }
